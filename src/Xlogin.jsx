abebd4b78c9daa137f7e8ed5b878497c4aa53f49
Inamul Haq <ihaq81285@gmail.com>
2024-02-26 01:11:47 +0530

    commit

--- src/Xlogin.jsx
+++ src/Xlogin.jsx
@@ -1,40 +1,44 @@
-import React from "react";
-import { useState } from "react";
+import React, { useState } from "react";
 
-export default function Xlogin(){
-    const [username,setUserName] = useState('');
-    const [password,setPassword] = useState('');
-    const [errorMessage, setErrorMessage] = useState('')
-    const [authorizedUser, setAuthorizedUser] = useState(false)
+export default function Xlogin() {
+    const [username, setUserName] = useState('');
+    const [password, setPassword] = useState('');
+    const [errorMessage, setErrorMessage] = useState('');
+    const [authorizedUser, setAuthorizedUser] = useState(false);
 
-   const handleFormSubmit =(e) =>{
+    const handleFormSubmit = (e) => {
         e.preventDefault();
-        if(username!=='user' && password!=='password'){
+        if (username !== 'user' || password !== 'password') {
             setErrorMessage('Invalid username or password');
-            setErrorMessage(<>Invalid username or password<br /><br /></>);
-        }
-        else{
+        } else {
             setAuthorizedUser(true);
         }
     }
 
-    return (<div>{authorizedUser?(<div>
-        <form onSubmit={handleFormSubmit}>
-            <h1>Login Page</h1>
-            <label>Welcome, {username}!
-            </label>
-        </form>
-    </div>):(<div>
-        <form onSubmit={handleFormSubmit}>
-            <h1>Login Page</h1>
-            {errorMessage}
-            <label>Username:
-                <input required type="text" value={username} onChange={(e) => {setUserName(e.target.value)}}/>
-            </label><br />
-            <label>Password:
-                <input required type="password" value={password} onChange={(e) => {setPassword(e.target.value)}}></input>
-            </label><br />
-            <button type="submit">Submit</button>
-        </form>
-    </div>)}</div>)
+    return (
+        <div>
+            {authorizedUser ? (
+                <div>
+                    <form onSubmit={handleFormSubmit}>
+                        <h1>Login Page</h1>
+                        <label>Welcome, {username}!</label>
+                    </form>
+                </div>
+            ) : (
+                <div>
+                    <form onSubmit={handleFormSubmit}>
+                        <h1>Login Page</h1>
+                        {errorMessage && <p>{errorMessage}<br /><br /></p>}
+                        <label>Username:
+                            <input required type="text" value={username} onChange={(e) => { setUserName(e.target.value) }} />
+                        </label><br />
+                        <label>Password:
+                            <input required type="password" value={password} onChange={(e) => { setPassword(e.target.value) }} />
+                        </label><br />
+                        <button type="submit">Submit</button>
+                    </form>
+                </div>
+            )}
+        </div>
+    );
 }
